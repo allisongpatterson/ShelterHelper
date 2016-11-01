0de0deebe35a16c3476345008b67e37f6b010dda
Allison Patterson <allison.patterson@students.olin.edu>
2016-11-01 16:19:15 -0700

    passing attribute specs

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,2 +1,15 @@
 class Shelter < ActiveRecord::Base
+  attr_accessor :name, :city, :phone
+
+  def initialize(name, city, phone)
+    @name = name
+    @city = city
+    @phone = phone
+  end
+
+  # temporary, for debugging and such
+  def display
+    p self.id
+    print 'name: #{name}, city: #{city}, phone: #{phone}'
+  end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Shelter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Shelter do
+
+  subject do
+    Shelter.new('Furry Friends Shelter', 'Sunnyvale', '123-456-7890')
+  end
+
+  describe 'we can access its attributes' do
+
+    it 'has the expected attributes' do
+      is_expected.to respond_to(:name)
+      is_expected.to respond_to(:city)
+      is_expected.to respond_to(:phone)
+    end
+
+    it 'doesn\'t have unexpected attributes' do
+      is_expected.not_to respond_to(:color)
+      is_expected.not_to respond_to(:parking)
+      is_expected.not_to respond_to(:security)
+    end
+
+  end
+  
 end
