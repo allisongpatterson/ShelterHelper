efb0a27982ee493d769eea293e15695d32165d9a
Allison Patterson <allison.patterson@students.olin.edu>
2016-12-13 12:31:36 -0800

    Found the 2 lines that were overwriting object parameters in the database!

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,19 +1,4 @@
 class Animal < ActiveRecord::Base
-  attr_accessor :name, :age, :sex
+  # attr_accessor :name, :age, :sex
   belongs_to :shelter
-
-  # def initialize(name, age, sex, shelter)
-  #   # note to self: @some_attribute is pretty
-  #   # much shorthand for self.some_attribute
-  #   # you can access it either way later
-  #   @name = name
-  #   @age = age
-  #   @sex = sex
-  #   @shelter = shelter
-  # end
-
-  # temporary, for debugging and such
-  def display
-    print 'name: ', @name, ', age: ', @age, ', sex: ', @sex
-  end
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,15 +1,4 @@
 class Shelter < ActiveRecord::Base
-  attr_accessor :name, :city, :phone
+  # attr_accessor :name, :city, :phone
   has_many :animals
-
-  # def initialize(name, city, phone)
-  #   @name = name
-  #   @city = city
-  #   @phone = phone
-  # end
-
-  # temporary, for debugging and such
-  def display
-    print 'name: #{name}, city: #{city}, phone: #{phone}'
-  end
 end
