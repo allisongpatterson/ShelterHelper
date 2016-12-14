e13d00e97410373e0222a714f87b7896fd953477
Allison Patterson <allison.patterson@students.olin.edu>
2016-12-14 12:17:26 -0800

    seeded data for animal and shelter index pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+shelters = [
+  { name: 'Second Chance', city: 'Sunnyvale', phone: '123-456-7890' },
+  { name: 'Fur Babies', city: 'Santa Clara', phone: '234-567-8901' },
+  { name: 'Pet World', city: 'Natik', phone: '345-678-9012' }
+]
+
+shelters.each do |attributes|
+  Shelter.create!(attributes)
+end
+
+animals = [
+  { name: 'Bruce', age: 4, sex: 'male', shelter_id: 1 },
+  { name: 'Mabel', age: 7, sex: 'female', shelter_id: 3 },
+  { name: 'Boots', age: 2, sex: 'male', shelter_id: 2 }
+]
+
+animals.each do |attributes|
+  Animal.create!(attributes)
+end
