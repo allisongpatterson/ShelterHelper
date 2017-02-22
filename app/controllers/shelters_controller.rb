d7fe3f37c41fb99c9f07230f34cac68190694094
Allison Patterson <allison.patterson@students.olin.edu>
2017-02-22 11:02:28 -0800

    implementing form for creating new shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -5,12 +5,25 @@ class SheltersController < ApplicationController
   end
 
   def create
-    #foo
+    @shelter = Shelter.new(shelter_params)
+    @shelter.save
+    redirect_to @shelter
+  end
+
+  def new
+    @shelter = Shelter.new
   end
 
   def show
     @shelter = Shelter.find(id=params[:id])
     @shelter_animals = Animal.where(shelter_id: params[:id])
+  end
+
+  private
+  # without this, submit will only create empty objects
+  # be sure to update these params if the model changes
+  def shelter_params
+    params.require(:shelter).permit(:name, :city, :phone)
   end
 
 end
